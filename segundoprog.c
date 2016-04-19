685f999aa6ccbc74ec5d8afa54036321197984c4
Fernando Fonseca <fernandofonsseca@gmail.com>
2016-04-19 00:59:47 +0000

    meu primeiro programa em github

---
+++ segundoprog.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("\nBoa noite!");
+    printf("\nMeu nome e Fernando");
+    printf("\nEste e meu segundo programa em C\n");
+    
+    
+    return 0;
+}
